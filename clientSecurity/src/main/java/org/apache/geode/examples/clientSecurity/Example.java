50e7ec1a6af98964ef685b7447513e577e3d16c9
Patrick Rhomberg <prhomberg@pivotal.io>
2017-09-18 11:05:32 -0700

    Additional streamlining of example.

--- clientSecurity/src/main/java/org/apache/geode/examples/clientSecurity/Example.java
+++ clientSecurity/src/main/java/org/apache/geode/examples/clientSecurity/Example.java
@@ -16,10 +16,9 @@ package org.apache.geode.examples.clientSecurity;
 
 import static org.apache.geode.distributed.ConfigurationProperties.SECURITY_CLIENT_AUTH_INIT;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang.Validate;
 import org.apache.logging.log4j.Logger;
 
 import org.apache.geode.cache.Region;
@@ -29,47 +28,39 @@ import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.internal.logging.LogService;
 
 public class Example implements AutoCloseable {
-  private static Logger logger = LogService.getLogger();
+  private static final Logger logger = LogService.getLogger();
+
   private static final String REGION1 = "region1";
   private static final String REGION2 = "region2";
 
-  private static final Map<String, String> REGION1_EXAMPLE_DATA = new HashMap<>();
-  private static final Map<String, String> REGION2_EXAMPLE_DATA = new HashMap<>();
-  static {
-    REGION1_EXAMPLE_DATA.put("'Soon I Will Be Invincible', Austin Grossman",
-        "Once you get past a certain threshold, everyone's problems are the same: fortifying your island and hiding the heat signature from your fusion reactor.");
-    REGION1_EXAMPLE_DATA.put("'The Name of the Wind', Patrick Rothfuss",
-        "You have to be a bit of a liar to tell a story the right way.");
-    REGION1_EXAMPLE_DATA.put("'The Lies of Locke Lamora', Scott Lynch",
-        "You can't help being young, but it's past time that you stopped being stupid.");
+  // Some example data
+  private static final String AUTHOR_GROSSMAN = "Grossman";
+  private static final String BOOK_BY_GROSSMAN = "Soon I Will Be Invincible";
 
-    REGION2_EXAMPLE_DATA.put("'Old Man's War', John Scalzi",
-        "I'm not insane, sir. I have a finely calibrated sense of acceptable risk.");
-    REGION2_EXAMPLE_DATA.put("'The Way of Kings', Brandon Sanderson",
-        "'Ah, the outdoors,' Shallan said. 'I visited that mythical place once.'");
-    REGION2_EXAMPLE_DATA.put("'The Blade Itself', Joe Abercrombie",
-        "But that was civilisation, so far as Logen could tell. People with nothing better to do, dreaming up ways to make easy things difficult.");
-  }
+  private static final String AUTHOR_ROTHFUSS = "Rothfuss";
+  private static final String BOOK_BY_ROTHFUSS = "The Name of the Wind";
 
-  private final String username;
-  private final String password;
+  private static final String AUTHOR_LYNCH = "Lynch";
+  private static final String BOOK_BY_LYNCH = "The Lies of Locke Lamora";
+
+  private static final String AUTHOR_SCALZI = "Scalzi";
+  private static final String BOOK_BY_SCALZI = "Old Man's War";
+
+  private static final String AUTHOR_SANDERSON = "Sanderson";
+  private static final String BOOK_BY_SANDERSON = "The Way of Kings";
+
+  private static final String AUTHOR_ABERCROMBIE = "Abercrombie";
+  private static final String BOOK_BY_ABERCROMBIE = "The Blade Itself";
 
   private final ClientCache cache;
   private final Region<String, String> region1;
   private final Region<String, String> region2;
 
-
   private Example(String username, String password) {
-    this.username = username;
-    this.password = password;
-
     Properties props = new Properties();
     props.setProperty(ExampleAuthInit.USER_NAME, username);
     props.setProperty(ExampleAuthInit.PASSWORD, password);
     props.setProperty(SECURITY_CLIENT_AUTH_INIT, ExampleAuthInit.class.getName());
-
-    // This cache is a singleton and must be closed before another instance of Example
-    // is instantiated with different credentials in the same VM.
 
     // connect to the locator using default port 10334
     cache = new ClientCacheFactory(props).setPoolSubscriptionEnabled(true)
@@ -80,71 +71,66 @@ public class Example implements AutoCloseable {
         .create(REGION2);
   }
 
-
-  /**
-   * A far-from-comprehensive example of integrated security in action.
-   */
   public static void main(String[] args) throws Exception {
+    adminUserCanPutAndGetEverywhere();
+    writeOnlyUserCannotGet();
+    readOnlyUserCannotPut();
+    regionUserIsRestrictedByRegion();
+  }
 
-    // dataReader is allowed to get but not put data on both regions
-    try (Example example = new Example("dataReader", "123")) {
-      example.putDataExample();
-      example.getDataExample();
+  private static void adminUserCanPutAndGetEverywhere() throws Exception {
+    String valueFromRegion;
+    try (Example example = new Example("superUser", "123")) {
+      // All puts and gets should pass
+      example.region1.put(AUTHOR_ABERCROMBIE, BOOK_BY_ABERCROMBIE);
+      example.region2.put(AUTHOR_GROSSMAN, BOOK_BY_GROSSMAN);
+
+      valueFromRegion = example.region1.get(AUTHOR_ABERCROMBIE);
+      Validate.isTrue(BOOK_BY_ABERCROMBIE.equals(valueFromRegion));
+
+      valueFromRegion = example.region2.get(AUTHOR_GROSSMAN);
+      Validate.isTrue(BOOK_BY_GROSSMAN.equals(valueFromRegion));
     }
+  }
 
-    // dataWriter is allowed to put data but will fail the get requests.
+  private static void writeOnlyUserCannotGet() {
     try (Example example = new Example("dataWriter", "123")) {
-      example.putDataExample();
-      example.getDataExample();
-    }
+      // Writes to any region should pass
+      example.region1.put(AUTHOR_LYNCH, BOOK_BY_LYNCH);
+      example.region2.put(AUTHOR_ROTHFUSS, BOOK_BY_ROTHFUSS);
 
-    // region1dataAdmin is allowed to both put and get data, but only in /region1
+      // This will fail since dataWriter does not have DATA:READ
+      example.region1.get(AUTHOR_LYNCH);
+    } catch (Exception e) {
+      logger.error("This exception should be caused by NotAuthorizedException", e);
+    }
+  }
+
+  private static void readOnlyUserCannotPut() {
+    try (Example example = new Example("dataReader", "123")) {
+      // This will pass
+      example.region1.get(AUTHOR_LYNCH);
+      example.region2.get(AUTHOR_ROTHFUSS);
+
+      // This will fail since dataReader does not have DATA:WRITE
+      example.region1.put(AUTHOR_SANDERSON, BOOK_BY_SANDERSON);
+    } catch (Exception e) {
+      logger.error("This exception should be caused by NotAuthorizedException", e);
+    }
+  }
+
+  private static void regionUserIsRestrictedByRegion() {
     try (Example example = new Example("region1dataAdmin", "123")) {
-      example.putDataExample();
-      example.getDataExample();
-    }
-  }
+      // This user can read and write only in region1
+      example.region1.put(AUTHOR_SANDERSON, BOOK_BY_SANDERSON);
+      String valueFromRegion = example.region1.get(AUTHOR_SANDERSON);
+      Validate.isTrue(BOOK_BY_SANDERSON.equals(valueFromRegion));
 
-  /** Attempt to put REGION1_EXAMPLE_DATA into /region1 and REGION2_EXAMPLE_DATA into /region2 */
-  private void putDataExample() {
-    logger.info("Writing example data to /region1 as user " + username);
-    try {
-      region1.putAll(REGION1_EXAMPLE_DATA);
-      logger.info("Writing complete.");
+      // This will fail since dataReader does not have DATA:WRITE:region2
+      example.region2.put(AUTHOR_SCALZI, BOOK_BY_SCALZI);
     } catch (Exception e) {
-      logger.info("Writing to /region1 as user '" + username + "' threw an exception.", e);
+      logger.error("This exception should be caused by NotAuthorizedException", e);
     }
-
-    logger.info("Writing example data to /region2 as user " + username);
-    try {
-      region2.putAll(REGION2_EXAMPLE_DATA);
-      logger.info("Writing complete.");
-    } catch (Exception e) {
-      logger.info("Writing to /region2 as user '" + username + "' threw an exception.", e);
-    }
-  }
-
-
-  /** Attempt to read data from both /region1 and /region2 */
-  private void getDataExample() {
-    logger.info("Reading data from /region1 as user " + username);
-    try {
-      region1.getAll(REGION1_EXAMPLE_DATA.keySet()).forEach(this::logEntry);
-    } catch (Exception e) {
-      logger.info("Reading from /region1 as user '" + username + "' threw an exception.", e);
-    }
-
-    logger.info("Reading data from /region2 as user " + username);
-    try {
-      region2.getAll(REGION2_EXAMPLE_DATA.keySet()).forEach(this::logEntry);
-    } catch (Exception e) {
-      logger.info("Reading from /region2 as user '" + username + "' threw an exception.", e);
-    }
-  }
-
-  private void logEntry(String key, String value) {
-    logger.info(String.format("\n\"%s\"\n -- %s", value, key));
-
   }
 
   /**
