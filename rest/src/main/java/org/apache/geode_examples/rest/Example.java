2d2f1b1b2362be218ea9af3e610a29abf84a00ff
ashish choudhary <aashish.choudhary1@gmail.com>
2020-12-21 02:58:20 +0530

    GEODE-8751: changes to use old HttpURLConnection API instead of JDK11 new HttpClient API

--- rest/src/main/java/org/apache/geode_examples/rest/Example.java
+++ rest/src/main/java/org/apache/geode_examples/rest/Example.java
@@ -14,53 +14,65 @@
  */
 package org.apache.geode_examples.rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Example {
   private static final String GEODE_REST_END_POINT = "http://localhost:8080/gemfire-api/v1/";
-  private static final String QUERY_PARAMETER = "?limit=ALL";
-  private static final String data = "{\"name\": \"Dan Smith\", \"technology\": \"Java\"}";
+  private static final String GET_REQUEST_PARAMETER = "?limit=ALL";
+  private static final String POST_REQUEST_PARAMETER = "?key=1";
+  private static final String DATA = "{\"name\": \"Dan Smith\", \"technology\": \"Java\"}";
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    var httpClient = createHttpClient();
-    HttpRequest postRequest = createHttpPostRequest();
-    sendHttpPostRequest(httpClient, postRequest);
-    HttpRequest getRequest = createHttpGetRequest();
-    sendHttpGetRequest(httpClient, getRequest);
+  public static void main(String[] args) throws IOException {
+    var httpPostRequestConnection = createHttpPostRequest();
+    writeData(httpPostRequestConnection);
+    var httpGetRequestConnection = createHttpGetRequest();
+    printValues(httpGetRequestConnection);
   }
 
-  private static HttpRequest createHttpPostRequest() {
-    return HttpRequest.newBuilder().uri(URI.create(GEODE_REST_END_POINT + "example-region"))
-        .header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(data))
-        .build();
+  private static HttpURLConnection createHttpGetRequest() throws IOException {
+    URL url = new URL(GEODE_REST_END_POINT + "example-region" + GET_REQUEST_PARAMETER);
+    HttpURLConnection httpURLConnection = getHttpURLConnection(url);
+    httpURLConnection.setRequestMethod("GET");
+    httpURLConnection.setRequestProperty("Accept", "application/json");
+    return httpURLConnection;
   }
 
-  private static HttpRequest createHttpGetRequest() {
-    return HttpRequest.newBuilder()
-        .uri(URI.create(GEODE_REST_END_POINT + "example-region" + QUERY_PARAMETER)).GET().build();
+  private static void printValues(HttpURLConnection conn) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())))) {
+      String response;
+      while ((response = br.readLine()) != null) {
+        System.out.println(response);
+      }
+    } finally {
+      conn.disconnect();
+    }
   }
 
-  static void sendHttpPostRequest(HttpClient httpClient, HttpRequest httpRequest)
-      throws IOException, InterruptedException {
-    HttpResponse<String> httpResponse =
-        httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-    System.out.println("Response status code " + httpResponse.statusCode());
+  private static HttpURLConnection createHttpPostRequest() throws IOException {
+    URL url = new URL(GEODE_REST_END_POINT + "example-region" + POST_REQUEST_PARAMETER);
+    HttpURLConnection httpURLConnection = getHttpURLConnection(url);
+    httpURLConnection.setRequestMethod("POST");
+    httpURLConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+    httpURLConnection.setRequestProperty("Accept", "application/json");
+    httpURLConnection.setDoOutput(true);
+    return httpURLConnection;
   }
 
-  static void sendHttpGetRequest(HttpClient httpClient, HttpRequest httpRequest)
-      throws IOException, InterruptedException {
-    HttpResponse<String> httpResponse =
-        httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-    System.out.println("Response status code " + httpResponse.statusCode());
-    System.out.println("Response body " + httpResponse.body());
+  private static HttpURLConnection getHttpURLConnection(URL url) throws IOException {
+    return (HttpURLConnection) url.openConnection();
   }
 
-  private static HttpClient createHttpClient() {
-    return HttpClient.newBuilder().build();
+  private static void writeData(HttpURLConnection conn) throws IOException {
+    try (OutputStream outputStream = conn.getOutputStream()) {
+      outputStream.write(DATA.getBytes(StandardCharsets.UTF_8));
+      conn.getInputStream();
+    }
   }
 }
 
